f12e40f5cdc392bd2dd4c8f6a8d8b2471cef7dd7
Mahmoud Fouda <youmcold@gmail.com>
2022-11-02 10:06:38 +0200

    new edit
    myapp - portofolio

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -2,12 +2,12 @@ import './footer.css';
 import whatsappBtn from './images/whatsapp_btn.png'
 
 const footer = () => {
-    let whatsappLink = 'https://wa.link/ub03n6';
+    let whatsappLin = 'https://wa.link/ub03n6';
     return (<>
     <div className="foot">
         <p className='des'>copyright@{new Date().getFullYear()}</p>
         <div className='floatingWhatsappBtn'>
-            <a href={whatsappLink}>
+            <a href={whatsappLin}>
                 <img src={whatsappBtn}></img>
                 <p></p>
                 </a>
